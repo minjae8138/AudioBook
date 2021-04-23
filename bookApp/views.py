d20e9eea85966541277c35de88ca48dfcce10e98
neroomi <skjung247@gmail.com>
2021-04-23 21:03:13 +0900

    Add login session

--- bookApp/views.py
+++ bookApp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render , redirect
@@ -9,11 +10,11 @@ import re
 import pandas as pd
 import numpy as np
 
-# 모델
-from konlpy.tag import Komoran
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from keras.models import load_model
+# # 모델
+# from konlpy.tag import Komoran
+# from tensorflow.keras.preprocessing.text import Tokenizer
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
+# from keras.models import load_model
 
 
 
@@ -30,16 +31,65 @@ def test(request):
     return render(request, 'test.html', {'posts':users})
 
 
-# 모델로드 및 세팅
-model = load_model('nlp_model.h5')
-tokenizer = Tokenizer()
-stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
-kor = Komoran()
+# # 모델로드 및 세팅
+# model = load_model('nlp_model.h5')
+# tokenizer = Tokenizer()
+# stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+# kor = Komoran()
 
-# 기본페이지
+# 기본페이지, 세션유지
+
+
+# def showName(request):
+#     if request.session.get('user_id') and request.session.get('name'):
+#         user = UserTb.objects.all()
+#         context = {'user_id' : request.session['user_id'],
+#                    'name' : request.session['name']}
+#         print('logged in - ', context['user_id'])
+#         return render(request, 'page1.html', context)
+#     else:
+#         # form = LoginForm()
+#         # return redirect('login')
+#         print('login needed - ', request.session['user_id'])
+#         return render(request, 'page1.html')
+
+
 def index(request):
+    if request.session.get('user_id') and request.session.get('name'):
+        users = UserTb.objects.all()
+        context = {'users':users}
+        print('logged in - ')
+        return render(request, 'page1.html', context)
+    else:
+        # form = LoginForm()
+        # return redirect('login')
+        print('login needed - ', request.session['user_id'])
+        return render(request, 'page1.html')
 
-    return render(request, 'page1.html')
+
+#
+# def index(request):
+#     if request.session.get('user_id') and request.session.get('name'):
+#         context = {'user_id' : request.session['user_id'],
+#                    'name' : request.session['name']}
+#         print('logged in - ', context['user_id'])
+#         return render(request, 'page1.html', context)
+#     else:
+#         # form = LoginForm()
+#         # return redirect('login')
+#         print('login needed - ', request.session['user_id'])
+#         return render(request, 'page1.html')
+
+
+
+#
+# def read(request):
+#     contents = ContentTb.objects.all()
+#     context = {'contents': contents}
+#     return render(request, 'page2.html', context)
+
+
+
 
 # 리스트형식 개행제거 함수
 def del_useless(text_list, del_list):
@@ -74,6 +124,8 @@ def pred(test_file):
     return test_file
 
 # 파일 업로드
+# 로그인 데코레이터 추가
+@login_required
 def upload(request) :
     file = request.FILES['text']
     # print('request upload - ',file)
@@ -89,6 +141,7 @@ def upload(request) :
     r_cont = re.sub("\n", "", r_cont)
     mid = re.split('[".]', r_cont)
     fin_list = del_useless(mid, ['', ' ', '  ' '.', "“", "”"])
+
     fin = fin_list[:]
 
     # 모델 적용 후 DB에 저장
@@ -122,9 +175,17 @@ def upload(request) :
     return redirect('index')
 
 
-# 음성서비스 페이지
+# 음성서비스 페이지, 세션 유지
 def read(request):
-    return render(request, 'page2.html')
+    if request.session.get('user_id') and request.session.get('name'):
+        context = {'user_id' : request.session['user_id'],
+                   'name' : request.session['name']}
+        print('logged in - ', context['user_id'])
+        return render(request, 'page2.html', context)
+    else:
+        print('login needed - ', request.session.get('user_id'))
+        return render(request, 'login.html')
+    # return render(request, 'page2.html')
 
 
 
@@ -145,3 +206,74 @@ def signup(request):
         form = UserForm(None)
         return render(request, 'signup.html', {'form':form})
 
+
+
+# 로그인
+from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import UserForm, LoginForm
+from django.contrib.auth import login, authenticate
+
+
+
+def login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        user_id = request.POST['user_id']
+        pw = request.POST['pw']
+        # 앞이 db 컬럼, 뒤가 받아오는 정보
+        user = UserTb.objects.get(user_id = user_id, pw = pw)
+        print('user result - ', user)
+
+        context={}
+        if user is not None:
+            request.session['user_id'] = user.user_id
+            request.session['name'] = user.name
+            context['user_id'] = request.session['user_id']
+            context['name'] = request.session['name']
+            print('로그인 성공')
+            print('user name - ', context['name'])
+            return redirect('index')
+
+        else:
+            print('로그인 실패')
+            return redirect('login')
+
+            # return HttpResponse('로그인 실패')
+        #
+        # else:
+        #     res_data = {}
+        #     if not (user_id and pw):
+        #         res_data['error'] = '모든 값을 입력해주세요.'
+        #         print(res_data['error'])
+        #
+        #     else:
+        #         user_id = UserTb.objects.get(user_id = user_id)
+        #
+        #         if check_password(pw, user_id.pw):
+        #             pass
+        #         else:
+        #             res_data['error'] = '비밀번호가 일치하지 않습니다.'
+        #             print(res_data['error'])
+        #
+        #     return render(request, 'login.html', res_data)
+
+    else:
+        form = LoginForm(None)
+        return render(request, 'login.html', {'form':form})
+
+
+
+
+
+
+# 로그아웃
+def logout(request):
+    request.session['user_id'] = {}
+    request.session['pw'] = {}
+    request.session.modified = True
+    print('user_id - ', request.session['user_id'])
+    print('로그아웃 성공')
+
+    return redirect('index')
